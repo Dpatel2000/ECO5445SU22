1253d0a86ca2361e818b682f37e4ee145a034466
Dpatel2000 <darshanpatelcpa@knights.ucf.edu>
2022-07-09 11:59:30 -0400

    Completed Assignments

---
+++ Assignment04/Assignment04.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  9 10:29:22 2022
+
+@author: darshanpatel
+"""
+
+import numpy as np
+
+NumberofTrials = int(10E4)
+Radius = 1
+
+Center = 0
+
+Xcoordinate = np.random.default_rng().uniform(-1, 1, (NumberofTrials,))
+Ycoordinate= np.random.default_rng().uniform(-1, 1, (NumberofTrials,))
+
+for n in range(NumberofTrials):
+    x = Xcoordinate[n]
+    y = Ycoordinate[n]
+    
+    if x**2 + y**2 <= Radius**2:
+        Center = Center + 1
+        
+area = 4*Center/NumberofTrials
+print(area)
